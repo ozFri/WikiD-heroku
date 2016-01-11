6f7bf89bece91b73dd1509bb44cfcba5b3aa9c80
OzFri <ozfraier@gmail.com>
2016-01-11 20:46:25 +0200

    renaming blog as Wikid, removing blogpost fields from index.html leaving only title

---
+++ WikiD/models.py
@@ -0,0 +1,118 @@
+from py2neo import Graph, Node, Relationship, authenticate
+from passlib.hash import bcrypt
+from datetime import datetime
+import os
+import uuid
+
+url = os.environ.get('GRAPHENEDB_URL', 'http://localhost:7474')
+username = os.environ.get('NEO4J_USERNAME')
+password = os.environ.get('NEO4J_PASSWORD')
+
+if username and password:
+    authenticate(url.strip('http://'), username, password)
+
+graph = Graph(url + '/db/data/')
+
+class User:
+    def __init__(self, username):
+        self.username = username
+
+    def find(self):
+        user = graph.find_one("User", "username", self.username)
+        return user
+
+    def register(self, password):
+        if not self.find():
+            user = Node("User", username=self.username, password=bcrypt.encrypt(password))
+            graph.create(user)
+            return True
+        else:
+            return False
+
+    def verify_password(self, password):
+        user = self.find()
+        if user:
+            return bcrypt.verify(password, user['password'])
+        else:
+            return False
+
+    def add_post(self, title, tags, text):
+        user = self.find()
+        post = Node(
+            "Post",
+            id=str(uuid.uuid4()),
+            title=title,
+            text=text,
+            timestamp=timestamp(),
+            date=date()
+        )
+        rel = Relationship(user, "PUBLISHED", post)
+        graph.create(rel)
+
+        tags = [x.strip() for x in tags.lower().split(',')]
+        for t in set(tags):
+            tag = graph.merge_one("Tag", "name", t)
+            rel = Relationship(tag, "TAGGED", post)
+            graph.create(rel)
+
+    def like_post(self, post_id):
+        user = self.find()
+        post = graph.find_one("Post", "id", post_id)
+        graph.create_unique(Relationship(user, "LIKED", post))
+
+    def get_recent_posts(self):
+        query = """
+        MATCH (user:User)-[:PUBLISHED]->(post:Post)<-[:TAGGED]-(tag:Tag)
+        WHERE user.username = {username}
+        RETURN post, COLLECT(tag.name) AS tags
+        ORDER BY post.timestamp DESC LIMIT 5
+        """
+
+        return graph.cypher.execute(query, username=self.username)
+
+    def get_similar_users(self):
+        # Find three users who are most similar to the logged-in user
+        # based on tags they've both blogged about.
+        query = """
+        MATCH (you:User)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag:Tag),
+              (they:User)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag)
+        WHERE you.username = {username} AND you <> they
+        WITH they, COLLECT(DISTINCT tag.name) AS tags, COUNT(DISTINCT tag) AS len
+        ORDER BY len DESC LIMIT 3
+        RETURN they.username AS similar_user, tags
+        """
+
+        return graph.cypher.execute(query, username=self.username)
+
+    def get_commonality_of_user(self, other):
+        # Find how many of the logged-in user's posts the other user
+        # has liked and which tags they've both blogged about.
+        query = """
+        MATCH (they:User {username: {they} })
+        MATCH (you:User {username: {you} })
+        OPTIONAL MATCH (they)-[:LIKED]->(post:Post)<-[:PUBLISHED]-(you)
+        OPTIONAL MATCH (they)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag:Tag),
+                       (you)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag)
+        RETURN COUNT(DISTINCT post) AS likes, COLLECT(DISTINCT tag.name) AS tags
+        """
+
+        return graph.cypher.execute(query, they=other.username, you=self.username)[0]
+
+def get_todays_recent_posts():
+    query = """
+    MATCH (user:User)-[:PUBLISHED]->(post:Post)<-[:TAGGED]-(tag:Tag)
+    WHERE post.date = {today}
+    RETURN user.username AS username, post, COLLECT(tag.name) AS tags
+    ORDER BY post.timestamp DESC LIMIT 5
+    """
+
+    return graph.cypher.execute(query, today=date())
+
+def timestamp():
+    epoch = datetime.utcfromtimestamp(0)
+    now = datetime.now()
+    delta = now - epoch
+    return delta.total_seconds()
+
+def date():
+    return datetime.now().strftime('%F')
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from blog import app
+from WikiD import app
 import os
 
 app.secret_key = os.urandom(24)
