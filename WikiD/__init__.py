9128db1b76a7e29d5a29fcdba6211b85c2edee52
Uria <uria@imrapid.com>
2016-02-10 12:35:48 +0200

    __init

--- WikiD/__init__.py
+++ WikiD/__init__.py
@@ -1,15 +1,20 @@
 from .views import app
 from .models import graph
 
+graphNodes = [("User", "username"),
+              ("Tag", "name"),
+              ("Post", "id"),
+              ("Inode", "id"),
+              ("Enode", "id"),
+              ("Snode", "id"),
+              ("Vote", "id")]
+
+
 def create_uniqueness_constraint(label, property):
     query = "CREATE CONSTRAINT ON (n:{label}) ASSERT n.{property} IS UNIQUE"
     query = query.format(label=label, property=property)
     graph.cypher.execute(query)
 
-create_uniqueness_constraint("User", "username")
-create_uniqueness_constraint("Tag", "name")
-create_uniqueness_constraint("Post", "id")
-create_uniqueness_constraint("Inode", "id")
-create_uniqueness_constraint("Enode", "id")
-create_uniqueness_constraint("Snode", "id")
-create_uniqueness_constraint("Vote", "id")
+
+for l, p in graphNodes:
+    create_uniqueness_constraint(l, p)
