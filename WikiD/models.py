799bbd0a1341876f5c4c67a63a552d748b863166
oz <ozfraier@gmail.com>
2018-02-25 23:34:38 +0200

    trying to connect to Graphenedb using Bolt

--- WikiD/models.py
+++ WikiD/models.py
@@ -7,13 +7,10 @@ import uuid
 from urllib.parse import urlparse, urlunparse
 
 
-url = urlparse(os.environ.get("GRAPHENEDB_URL","http://localhost:7474"))
-url_without_auth = urlunparse((url.scheme, "{0}:{1}".format(url.hostname, url.port), '', None, None, None))
-user = url.username
-password = url.password
-
-authenticate(url_without_auth, user, password)
-graph = Graph(url_without_auth, bolt = False)
+graphenedb_url = os.environ.get("GRAPHENEDB_BOLT_URL")
+graphenedb_user = os.environ.get("GRAPHENEDB_BOLT_USER")
+graphenedb_pass = os.environ.get("GRAPHENEDB_BOLT_PASSWORD")
+graph = Graph(graphenedb_url, user=graphenedb_user, password=graphenedb_pass, bolt = True, secure = True, http_port = 24789, https_port = 24780)
 
 
 def create_new_vote(user, vote_type, event, node):
