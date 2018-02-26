aa5fa54b381b289c106fe9333c3259002ece4009
oz <ozfraier@gmail.com>
2018-02-26 17:20:38 +0200

    authenticating with bolt only

--- WikiD/models.py
+++ WikiD/models.py
@@ -5,10 +5,10 @@ from datetime import datetime
 import os
 import uuid
 
-#GRAPHENEDB_URL=https://app45980694-1SqesO:b.2Wbuj0oq2sYy.zJWPkmzpJi7O3G4U@
+#GRAPHENEDB_URL=https://app45980694-1SqesO:b.2Wbuj0oq2sYy.zJWPkmzpJi7O3G4U@hobby-hankpoiekhacgbkegaaijpal.dbs.graphenedb.com:24780
 
-authenticate("hobby-hankpoiekhacgbkegaaijpal.dbs.graphenedb.com:24789", "app45980694-1SqesO", "b.2Wbuj0oq2sYy.zJWPkmzpJi7O3G4U")
-graph = Graph("http://hobby-hankpoiekhacgbkegaaijpal.dbs.graphenedb.com:24780", bolt = False)
+#authenticate("hobby-hankpoiekhacgbkegaaijpal.dbs.graphenedb.com:24789", "app45980694-1SqesO", "b.2Wbuj0oq2sYy.zJWPkmzpJi7O3G4U")
+graph = Graph("bolt://hobby-hankpoiekhacgbkegaaijpal.dbs.graphenedb.com:24786 ", bolt = True ,user = "app45980694-1SqesO", password = "b.2Wbuj0oq2sYy.zJWPkmzpJi7O3G4U")
 
 
 def create_new_vote(user, vote_type, event, node):
