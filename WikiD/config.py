0aa62acd8ce555cdba2f9619834b5cc5fc4400c9
Ghassan <ghassan.maslamani@gmail.com>
2018-05-31 21:35:17 +0300

    Supporting graphql API Relates #22

--- WikiD/config.py
+++ WikiD/config.py
@@ -1,5 +1,12 @@
 user = "neo4j"
-password ="1234"
+password ="12345"
 db_host_port = "localhost:7473"
 db_url = "https://%s/db/data" % (db_host_port)
+# This for username=neo4j, password='12345'
+# Link to generate https://www.blitter.se/utils/basic-authentication-header-generator/
+headers={'Authorization': 'Basic bmVvNGo6MTIzNDU='}
+
+#link to neo4j graphql:
+graphql_url='http://localhost:7474/graphql/'
+
 is_secured = True
