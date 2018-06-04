918f4dca4f2514d4b5c1e00ff8fed26e3ca8a39a
oz <ozfraier@gmail.com>
2018-06-04 23:50:34 +0300

    edited config files with default remote settings

--- WikiD/config.py
+++ WikiD/config.py
@@ -1,12 +1,20 @@
-user = "neo4j"
-password ="12345"
-db_host_port = "localhost:7473"
-db_url = "https://%s/db/data" % (db_host_port)
+#LOCAL SETTINGS
+#user = "neo4j"
+#password ="neo4j1"
+#db_host_port = "7473"
+#db_url =  "localhost:" % (db_host_port)
+#headers={'Authorization': 'Basic bmVvNGo6MTIzNDU='}
+
+#REMOTE SETTINGS
+user = "app45980694-1SqesOw"
+password ="b.XYhdVctgD8b0.jsxxlTdB848loItn"
+db_host_port = "24786"
+db_url =  "bolt://hobby-hankpoiekhacgbkegaaijpal.dbs.graphenedb.com:" % (db_host_port)
 # This for username=neo4j, password='12345'
 # Link to generate https://www.blitter.se/utils/basic-authentication-header-generator/
-headers={'Authorization': 'Basic bmVvNGo6MTIzNDU='}
+headers={'Authorization': 'Basic YXBwNDU5ODA2OTQtMVNxZXNPdzo9ImIuWFloZFZjdGdEOGIwLmpzeHhsVGRCODQ4bG9JdG4='}
 
 #link to neo4j graphql:
-graphql_url='http://localhost:7474/graphql/'
+graphql_url= db_url + '/graphql/'
 
 is_secured = True
