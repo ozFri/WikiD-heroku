08f0420636f67fd20f671689cb0cc8d61c045267
oz <ozfraier@gmail.com>
2018-07-10 16:01:01 +0300

    modified configuration file to use classes

--- WikiD/config.py
+++ WikiD/config.py
@@ -5,6 +5,10 @@ class config:
     db_host_port = "localhost:7473"
     db_url = "https://%s/db/data" % (db_host_port)
     headers={'Authorization': 'Basic bmVvNGo6MTIzNDU='}
+    #link to neo4j graphql:
+    graphql_url= db_url + '/graphql/'
+
+    is_secured = True
 
 #REMOTE SETTINGS
 class heroku(config):
@@ -16,7 +20,3 @@ class heroku(config):
     # Link to generate https://www.blitter.se/utils/basic-authentication-header-generator/
     headers={'Authorization': 'b.Dn3Ho4YY9uNa.OI0SJoL7PTg0HLoq'}
 
-#link to neo4j graphql:
-graphql_url= db_url + '/graphql/'
-
-is_secured = True
